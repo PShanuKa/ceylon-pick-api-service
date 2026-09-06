42b9d30f14486f8549cbcaa124c81b95bb6470df
Pasindu shanuka <143288490+PShanuKa@users.noreply.github.com>
2026-09-06 19:16:33 +0530

    init project

---
+++ src/main/java/com/ceylon_pick/ceylon/CeylonApplication.java
@@ -0,0 +1,13 @@
+package com.ceylon_pick.ceylon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeylonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeylonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceylon_pick/ceylon/CeylonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceylon_pick.ceylon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeylonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
